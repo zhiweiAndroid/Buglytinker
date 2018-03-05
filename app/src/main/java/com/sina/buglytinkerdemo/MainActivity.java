82f92a73ea4d6ded551850005cae973a38220a4c
zitan <1213082309@qq.com>
2018-03-05 18:01:03 +0800

    2018-03-05 测试提交

--- app/src/main/java/com/sina/buglytinkerdemo/MainActivity.java
+++ app/src/main/java/com/sina/buglytinkerdemo/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
